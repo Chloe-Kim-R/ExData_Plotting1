79e9509f4121429c0dfa10f9ca4be787ee4aee72
Chloe-Kim-R <chloe.hjkim11@gmail.com>
2020-02-06 18:17:54 +0900

    R script for plot2

---
+++ plot2.R
@@ -0,0 +1,18 @@
+##Load data set 
+data <- read.table("household_power_consumption.txt",header=TRUE, sep=";")
+sub <- subset(data,Date=="1/2/2007" | Date=="2/2/2007" )
+sub$Global_active_power <- as.numeric(sub$Global_active_power)
+sub$Date <-as.Date(sub$Date,"%d/%m/%Y")
+dt<- paste(sub$Date,sub$Time)
+sub$Time <- strptime(dt,"%Y-%m-%d %H:%M:%S")
+
+## Plot 2
+dev.copy(png,"plot2.png", width = 480, height = 480)
+plot(sub$Time,sub$Global_active_power,type="l", ylab="Global Active Power(killowatts)",xlab="")
+dev.off() 
+
+
+ 
+ 
+ 
+        
