b56be8e590b05de805ae811cee71468153fcf01e
Chloe-Kim-R <chloe.hjkim11@gmail.com>
2020-02-07 09:15:41 +0900

    R script for Plot1

--- plot1.R
+++ plot1.R
@@ -1,12 +1,11 @@
 ##Load data set 
-data <- read.table("household_power_consumption.txt",header=TRUE, sep=";")
-sub <- subset(data,Date=="1/2/2007" | Date=="2/2/2007"  )
+data <- read.table("household_power_consumption.txt",header=TRUE, sep=";",stringAsFactors=FALSE)
+sub <- subset(data,Date=="1/2/2007" | Date=="2/2/2007" )
+sub[,3:9] <- lapply(sub[,3:9], function(x) as.numeric(as.character(x)))
 
 ## Plot 1 
 dev.copy(png,"plot1.png", width = 480, height = 480)
-sub$Global_active_power <- as.numeric(sub$Global_active_power)
-hist(sub$Global_active_power, col="red", xlab="Global Active Power (kilowatt)", main="Global Active Power")
+sub$Global_active_power <- as.numeric(as.character(sub$Global_active_power))
 dev.off()
-
 
 
